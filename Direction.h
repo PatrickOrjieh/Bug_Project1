571cfe2430aee474f9cdde0557be68900c5d4b5b
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-03-28 17:04:20 +0100

    Added Direction.h for General use

---
+++ Direction.h
@@ -0,0 +1,15 @@
+//
+// Created by orjie on 28/03/2023.
+//
+
+#ifndef BUG_PROJECT1_DIRECTION_H
+#define BUG_PROJECT1_DIRECTION_H
+
+enum Direction {
+    North = 1,
+    East = 2,
+    South = 3,
+    West = 4
+};
+
+#endif //BUG_PROJECT1_DIRECTION_H
