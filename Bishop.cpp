38b649bab4822a3667c058218d3e7dcda84e84d0
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-04-14 02:26:15 +0100

    Move Done for Bishop

--- Bishop.cpp
+++ Bishop.cpp
@@ -37,7 +37,82 @@ void Bishop::setBisLength(int b) {
 //- record new position in bishops path history
 //note that the bishop can only move diagonally
 void Bishop::move(){
-    //will implement later
+    //if the bug is at the edge of the board and can't move in the current direction, set a new direction at random
+    while (isWayBlocked()) {
+        int newDirection = rand() % 4 + 5;
+        setDirection((Direction) newDirection);
+    }
+
+    // get the current position, direction and path of the bug
+    std::pair<int, int> currentPosition = getPosition();
+    Direction currentDirection = getDirection();
+    std::list<std::pair<int, int>> currentPath = getPath();
+
+    // calculate the maximum distance the bug can hop in the current direction
+    //note that the bishop can only move diagonally
+    int maxHopDistance = 0;
+    switch (currentDirection) {
+        case NorthEast:
+            maxHopDistance = std::min(currentPosition.second, 9 - currentPosition.first);
+            break;
+        case SouthEast:
+            maxHopDistance = std::min(9 - currentPosition.second, 9 - currentPosition.first);
+            break;
+        case SouthWest:
+            maxHopDistance = std::min(9 - currentPosition.second, currentPosition.first);
+            break;
+        case NorthWest:
+            maxHopDistance = std::min(currentPosition.second, currentPosition.first);
+            break;
+    }
+
+    // if the bug can hop the full distance, move it and update its position
+    if (maxHopDistance >= bisLength) {
+        switch (currentDirection) {
+            case NorthEast:
+                currentPosition.first += bisLength;
+                currentPosition.second -= bisLength;
+                break;
+            case SouthEast:
+                currentPosition.first += bisLength;
+                currentPosition.second += bisLength;
+                break;
+            case SouthWest:
+                currentPosition.first -= bisLength;
+                currentPosition.second += bisLength;
+                break;
+            case NorthWest:
+                currentPosition.first -= bisLength;
+                currentPosition.second -= bisLength;
+                break;
+        }
+        setPosition(currentPosition);
+        currentPath.push_back(currentPosition);
+        setPath(currentPath);
+    } else {
+        // if the bug can't hop the full distance, move it to the edge and update its position
+        switch (currentDirection) {
+            case NorthEast:
+                currentPosition.first += maxHopDistance;
+                currentPosition.second -= maxHopDistance;
+                break;
+            case SouthEast:
+                currentPosition.first += maxHopDistance;
+                currentPosition.second += maxHopDistance;
+                break;
+            case SouthWest:
+                currentPosition.first -= maxHopDistance;
+                currentPosition.second += maxHopDistance;
+                break;
+            case NorthWest:
+                currentPosition.first -= maxHopDistance;
+                currentPosition.second -= maxHopDistance;
+                break;
+        }
+        setPosition(currentPosition);
+        currentPath.push_back(currentPosition);
+        setPath(currentPath);
+    }
 }
 
 //display the bug's information
